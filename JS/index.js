9d4b5747330b2d33323a1b137846a53acbb8a7e3
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-11-22 19:22:08 +0200

    wip

---
+++ JS/index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  // gets the buttons with they IDs
+  const previousBtn = document.getElementById('previousBtn');
+  const nextNumberBtn = document.getElementById('nextNumberBtn');
+
+  // Get the result element by its ID
+  const resultElement = document.getElementById('result');
+
+  // The initial first number to go next od previous from 
+  let currentNumber = 1;
+
+  // show number when user comes onto the page for the first time
+  resultElement.textContent = 'Result: ' + currentNumber;
+
+  // Add click event listeners to the buttons
+  previousBtn.addEventListener('click', function () {
+    // Call the previousNumber function to get the previous number
+    const previousNum = previousNumber(currentNumber);
+
+    // Update the number
+    currentNumber = previousNum;
+
+    // Display = result 
+    resultElement.textContent = 'Result: ' + currentNumber;
+  });
+
+  nextNumberBtn.addEventListener('click', function () {
+    // Call the nextNumber function to get the next number
+    const nextNum = nextNumber(currentNumber);
+
+    // Update the  number
+    currentNumber = nextNum;
+
+    // Display the result in the result element
+    resultElement.textContent = 'Result: ' + currentNumber;
+  });
+
+  //  nextNumber function
+  function nextNumber(currentNumber) {
+    // Assuming the input is a valid number
+    return currentNumber + 1;
+  }
+
+  // previousNumber function
+  function previousNumber(currentNumber) {
+    // Assuming the input is a valid number
+    return currentNumber - 1;
+  }
+});
